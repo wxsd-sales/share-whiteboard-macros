0c6986fc60c3836703eced345dcfa2ce416ae39f
wazquez <victorvazquezv@gmail.com>
2024-06-20 07:45:53 +0200

    Update share-whiteboard.js

--- share-whiteboard.js
+++ share-whiteboard.js
@@ -31,7 +31,7 @@ import xapi from 'xapi';
 const emailConfig = {
   destination: 'user@example.com', // Change this value to the email address you want the whiteboard to be sent to
   body: 'Here you have your white board', // Email body text of your choice, this is an example
-  subject: 'New white board', // Email suubject of your choice, this is an example
+  subject: 'New white board', // Email subject of your choice, this is an example
   attachmentFilename: 'myfile-companion-mode' // File name of your choice, this is an example
 };
 const buttonConfig = {
